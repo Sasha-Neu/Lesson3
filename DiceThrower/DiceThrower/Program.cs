a615ecfd2e30cc56fce900b1e6cde88f3528f132
Sasha <machete.pills@gmail.com>
2023-01-31 01:45:21 -0800

    Throwing dice

--- DiceThrower/DiceThrower/Program.cs
+++ DiceThrower/DiceThrower/Program.cs
@@ -7,14 +7,34 @@ namespace DiceThrower
     {
         static void Main(string[] args)
         {
-            IDice die1;
-            IDice die2;
+            //IDice die1;
+            //IDice die2;
 
-            die1 = new D20();
-            Console.WriteLine($"The number on die 1 is { die1.Throw() }.");
+            //die1 = new D20();
+            //Console.WriteLine($"The number on die 1 is { die1.Throw() }.");
 
-            die2 = new D6();
-            Console.WriteLine($"The number on die 2 is { die2.Throw() }.");
+            //die2 = new D6();
+            //Console.WriteLine($"The number on die 2 is { die2.Throw() }.");
+
+            Console.WriteLine("Pick a dice, please.");
+            var readString = Console.ReadLine();
+            IDice anyDice = null;
+
+            if (readString == "20")
+            {
+                anyDice = new D20();
+            }
+            else if (readString == "6")
+            {
+                anyDice = new D6();
+            }
+            else
+            {
+                Console.WriteLine("This die doesn't exist.");
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine($"The number on the die is { anyDice.Throw() }.");
         }
     }
 }
